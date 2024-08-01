3a1e915dd6bc5bd01356602cc09d7e3953eaedd8
evertonsavio <everluca@outlook.com>
2024-08-01 06:33:09 -0300

    :sparkles: add validators to bloc

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,12 +1,20 @@
 import 'dart:async';
+import 'package:flutter_bloc/src/blocs/validators.dart';
 
-class Bloc {
+class Bloc extends Validators {
+
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
   void Function(String) get changeEmail => _emailController.sink.add;
   void Function(String) get changePassword => _passwordController.sink.add;
 
-  Stream<String> get email => _emailController.stream;
-  Stream<String> get password => _passwordController.stream;
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password => _passwordController.stream.transform(validatePassword);
+
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
+
 }
